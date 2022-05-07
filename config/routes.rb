e8c8f34ad0e5c8f87204e7c79bda5246ffeac20c
Cire-w <104166692+Cire-w@users.noreply.github.com>
2022-05-07 16:14:01 +0200

    Add files via upload
    Ho aggiunto la pagina di login. Ha me da sempre il solito errore quando provo a lanciare il progetto. Verificare che non vi siano problemi nella visualizzazione nella pagina login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only:[:new,:create,:edit,:update,:show,:destroy]
+
+  get 'sign_in', to: "sessions#new"
+ post 'sign_in', to: "sessions#create"
+
 end
