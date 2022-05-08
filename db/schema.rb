99a7696c6a5b1b685c07c92785bc1cc7181dae8e
Emanuele <ema@localhost.localdomain>
2022-05-08 17:17:30 +0200

    On branch develop 	modified:   Gemfile 	modified:   Gemfile.lock 	new file:   app/models/user.rb 	new file:   app/views/devise/confirmations/new.html.erb 	new file:   app/views/devise/mailer/confirmation_instructions.html.erb 	new file:   app/views/devise/mailer/email_changed.html.erb 	new file:   app/views/devise/mailer/password_change.html.erb 	new file:   app/views/devise/mailer/reset_password_instructions.html.erb 	new file:   app/views/devise/mailer/unlock_instructions.html.erb 	new file:   app/views/devise/passwords/edit.html.erb 	new file:   app/views/devise/passwords/new.html.erb 	new file:   app/views/devise/registrations/edit.html.erb 	new file:   app/views/devise/registrations/new.html.erb 	new file:   app/views/devise/sessions/new.html.erb 	new file:   app/views/devise/shared/_error_messages.html.erb 	new file:   app/views/devise/shared/_links.html.erb 	new file:   app/views/devise/unlocks/new.html.erb 	modified:   config/initializers/devise.rb 	modified:   config/routes.rb 	modified:   db/schema.rb 	modified:   db/seeds.rb 	new file:   test/fixtures/users.yml 	new file:   test/models/user_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_104510) do
+ActiveRecord::Schema.define(version: 2022_05_08_150140) do
 
   create_table "condominios", force: :cascade do |t|
     t.string "condo_id"
@@ -24,6 +24,20 @@ ActiveRecord::Schema.define(version: 2022_05_07_104510) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["fk_superutenti_condo_id_id"], name: "index_condominios_on_fk_superutenti_condo_id_id"
     t.index ["fk_utenti_condo_id_id"], name: "index_condominios_on_fk_utenti_condo_id_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "uname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "condominios", "fk_superutenti_condo_ids"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+condominiii = [{ :condo_id => '12rfaw41', :nome_cond => 'testnome1', :comune_condo => 'sambuceto' }]
+
+condominiii.each do |strutt|
+	Condominio.create!(strutt)
+end
