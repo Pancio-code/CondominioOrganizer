3a1001171541ff6a08e2fafae37d36814527c56b
Emanuele <ema@localhost.localdomain>
2022-05-08 17:52:15 +0200

    Changes to be committed: 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_144130) do
+ActiveRecord::Schema.define(version: 2022_05_08_150140) do
 
   create_table "condominios", force: :cascade do |t|
     t.string "condo_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_05_08_144130) do
     t.string "confirmation_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
