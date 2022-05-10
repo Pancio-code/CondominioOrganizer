81ea2ddfd92841ff5f1825e6ae3d14c998f54570
Emanuele <ema@localhost.localdomain>
2022-05-10 15:32:39 +0200

    On branch develop  Your branch is up to date with 'origin/develop'.  Changes to be committed: 	modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :uname, presence :true, uniqueness: { case_sensitive: false }
+  validates :uname, presence: true, uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
