dd419b537e19fd6db3923ed111794c031b9d0703
Cire-w <104166692+Cire-w@users.noreply.github.com>
2022-05-26 15:00:06 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :condominios
   root 'welcome#index'
-  get 'sign_in', to: "sessions#new"
-  post 'sign_in', to: "sessions#create"
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
