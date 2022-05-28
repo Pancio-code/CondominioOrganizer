e1e76e6893ebe6563f4cc11522150e373257014c
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-05-28 12:23:08 +0200

    On branch develop  Your branch is up to date with 'origin/develop'.  Changes to be committed: 	modified:   db/development.sqlite3 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_100227) do
+ActiveRecord::Schema.define(version: 2022_05_28_102005) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_100227) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uname"
-    t.boolean "from_oauth"
+    t.boolean "from_oauth", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
