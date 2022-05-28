f8bd116c9506adc67a879d21a04414d7e900b05b
Andrea Panceri <andrea.pancio00@gmail.com>
2022-05-28 11:32:30 +0200

    red confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,5 @@ class User < ApplicationRecord
 
    has_one_attached :avatar
    validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
-              file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
+              file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif','image/jpg'] }
 end
