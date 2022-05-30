9b57971f90ba30d97edd1af99ff6c633ff19e6a3
Andrea Panceri <andrea.pancio00@gmail.com>
2022-05-30 20:08:48 +0200

    tutte le api

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,7 +275,7 @@ Devise.setup do |config|
     access_type: "offline",
     prompt: "consent",
     select_account: true,
-    scope: 'userinfo.email, calendar'
+    scope: 'userinfo.email, calendar,gmail.compose,drive'
   }
 
   # ==> Warden configuration
