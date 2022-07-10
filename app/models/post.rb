ce6f9e67b42a67b51e938478a7cce041075900f5
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-07-10 11:47:10 +0200

    ristrutturazione completa Post.
     On branch commentipost
     Changes to be committed:
    	modified:   app/assets/stylesheets/scaffolds.scss
    	modified:   app/controllers/comments_controller.rb
    	modified:   app/controllers/posts_controller.rb
    	modified:   app/models/post.rb
    	new file:   app/views/posts/_form.html.erb
    	new file:   app/views/posts/_post.json.jbuilder
    	new file:   app/views/posts/edit.html.erb
    	modified:   app/views/posts/index.html.erb
    	new file:   app/views/posts/index.json.jbuilder
    	new file:   app/views/posts/new.html.erb
    	modified:   app/views/posts/show.html.erb
    	new file:   db/migrate/20220710092412_dropposttable.rb
    	renamed:    db/migrate/20220628092650_create_posts.rb -> db/migrate/20220710092902_create_posts.rb
    	modified:   db/schema.rb
    	modified:   test/controllers/posts_controller_test.rb
    	modified:   test/fixtures/posts.yml
    	new file:   test/system/posts_test.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :condominio
   belongs_to :user
+  belongs_to :condominio
   has_many :comments
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 3 }
+  validates :body, presence:true, length: {minimum: 3}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_165708) do
+ActiveRecord::Schema.define(version: 2022_07_10_092902) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,8 +73,8 @@ ActiveRecord::Schema.define(version: 2022_07_07_165708) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "condominio_id", null: false
     t.integer "user_id", null: false
+    t.integer "condominio_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["condominio_id"], name: "index_posts_on_condominio_id"
