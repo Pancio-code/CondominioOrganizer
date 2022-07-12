59719db8c633d087a31dc53a11156f73235be233
Andrea Panceri <andrea.pancio00@gmail.com>
2022-07-12 14:52:16 +0200

    modifica eventi anche su calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -114,8 +114,76 @@ class EventsController < ApplicationController
     @event.categoria = params[:categoria]
     respond_to do |format|
       if @event.update(event_params)
-        @condomini.each do |condomino|
-          CondominioMailer.with(email: User.find(condomino.user_id).email ,message: @event.titolo,categoria: params[:categoria]).send_event_invitation(@event.start_time,current_user.id,@condominio.id).deliver_later
+        if current_user.from_oauth? && @event.calendar_id != nil
+          session_time = Time.now - session[:time_login].to_datetime
+          list_email = []
+          @condomini.each do |condomino|
+            utente_corrente_email = User.find(condomino.user_id).email
+            list_email << Google::Apis::CalendarV3::EventAttendee.new(email: utente_corrente_email)
+          end
+          require 'json' 
+          token, refresh_token = *JSON.parse(File.read('credentials.data'))
+          client = Signet::OAuth2::Client.new(client_id: Figaro.env.google_api_id,client_secret: Figaro.env.google_api_secret,access_token: token,refresh_token: refresh_token,token_credential_uri: 'https://accounts.google.com/o/oauth2/token',scope: 'calendar')
+          if client.expired? || (session_time/60).to_i > 28            
+            new_token = client.refresh!
+            @new_tokens =
+              {
+                :access_token  => new_token["access_token"],
+                :refresh_token => new_token["refresh_token"]
+              }
+            client.access_token  = @new_tokens[ :access_token ]
+            client.refresh_token = @new_tokens[ :refresh_token ]
+            File.write 'credentials.data', [client.access_token, client.refresh_token].to_json
+          end
+          service = Google::Apis::CalendarV3::CalendarService.new
+          service.authorization = client
+          
+          calendar = service.get_calendar(:primary)
+          begin service.get_event(calendar.id, @event.calendar_id)
+            service.get_event(calendar.id, @event.calendar_id)
+
+            if params[:categoria] == "pagamento"
+              event = Google::Apis::CalendarV3::Event.new(
+                start: Google::Apis::CalendarV3::EventDateTime.new(date_time: @event.start_time.to_datetime.rfc3339,time_zone: "Europe/Rome"),
+                end: Google::Apis::CalendarV3::EventDateTime.new(date_time: @event.start_time.to_datetime.rfc3339,time_zone: "Europe/Rome"),
+                summary: "Avviso " + params[:categoria],
+                location: @condominio.nome + ', ' + @condominio.comune + ', ' + @condominio.indirizzo,
+                description: @event.titolo,
+                attendees: list_email,
+                recurrence: [
+                  'RRULE:FREQ=WEEKLY'
+                ],
+                reminders: Google::Apis::CalendarV3::Event::Reminders.new(
+                  use_default: false,
+                  overrides: [
+                    Google::Apis::CalendarV3::EventReminder.new(
+                      reminder_method: 'email',
+                      minutes: 10080
+                    )
+                  ]
+                )
+              )
+            else
+              event = Google::Apis::CalendarV3::Event.new(
+                start: Google::Apis::CalendarV3::EventDateTime.new(date_time: @event.start_time.to_datetime.rfc3339,time_zone: "Europe/Rome"),
+                end: Google::Apis::CalendarV3::EventDateTime.new(date_time: @event.start_time.to_datetime.rfc3339,time_zone: "Europe/Rome"),
+                summary: "Avviso " + params[:categoria],
+                location: @condominio.nome + ', ' + @condominio.comune + ', ' + @condominio.indirizzo,
+                description: @event.titolo,
+                attendees: list_email,
+              )
+            end
+            service.update_event(calendar.id, @event.calendar_id, event,send_notifications: true)
+          rescue => e
+            if !e.status_code == 404
+              redirect_to root_path, :alert => e.message
+              return false
+            end
+          end
+        else
+          @condomini.each do |condomino|
+            CondominioMailer.with(email: User.find(condomino.user_id).email ,message: @event.titolo,categoria: params[:categoria]).send_event_invitation(@event.start_time,current_user.id,@condominio.id).deliver_later
+          end
         end
         format.html { redirect_to event_url(id: @event.id,condominio_id: @condominio.id), notice: "Evento modificato correttamente." }
         format.json { render :show, status: :ok, location: @event }
