6b566039a8f07de8aaa4789512d474b41f1a22bb
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-07-13 12:02:55 +0200

    On branch implem_drive2  Changes to be committed: 	modified:   app/models/condomino.rb 	modified:   app/models/gdrive_condo_item.rb

--- app/models/condomino.rb
+++ app/models/condomino.rb
@@ -1,4 +1,6 @@
 class Condomino < ApplicationRecord 
 	belongs_to :user
 	belongs_to :condominios, optional: true
+
+        has_many :gdrive_user_items, dependent: :delete_all
 end
--- app/models/gdrive_condo_item.rb
+++ app/models/gdrive_condo_item.rb
@@ -1,3 +1,4 @@
 class GdriveCondoItem < ApplicationRecord
   belongs_to :condominio
+  has_many :gdrive_user_items
 end
