4086adc58162cb0a674a8d0f02f1b7aa36323bc8
Andrea Panceri <andrea.pancio00@gmail.com>
2022-07-16 17:39:46 +0200

    porta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :port                 => 25,
     :user_name            => ENV["EMAIL_DI_SERVIZIO"],
     :password             => ENV["PASSWORD_EMAIL"],
     :authentication       => "plain",
