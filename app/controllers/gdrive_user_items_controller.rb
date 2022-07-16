acccbdb0bb050dac932ad25103e9b484a784b125
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-07-16 16:01:09 +0200

    On branch develop  Your branch is up to date with 'origin/develop'.
     Changes to be committed:
    	modified:   app/controllers/gdrive_user_items_controller.rb

--- app/controllers/gdrive_user_items_controller.rb
+++ app/controllers/gdrive_user_items_controller.rb
@@ -2,12 +2,12 @@ class GdriveUserItemsController < ApplicationController
   before_action :authenticate_user!
 
   def initialize_drive                                              
-    file = File.read('config/google_credentials.json')
+#   file = File.read('config/google_credentials.json')
     
     @service = Google::Apis::DriveV3::DriveService.new
     scope = 'https://www.googleapis.com/auth/drive'
     authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: StringIO.new(file), scope: scope)
+      json_key_io: StringIO.new(ENV[ 'GOOGLE_CREDENTIALS' ]), scope: scope)
   
     authorizer.fetch_access_token!
     @service.authorization = authorizer
