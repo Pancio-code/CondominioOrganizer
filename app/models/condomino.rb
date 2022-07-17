acfaa777356a583c4ab03d72616ec51f839ac0b5
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-07-17 10:58:49 +0200

    On branch develop  Your branch is up to date with 'origin/develop'.
     Changes to be committed:
    	modified:   app/models/condomino.rb

--- app/models/condomino.rb
+++ app/models/condomino.rb
@@ -1,5 +1,5 @@
 class Condomino < ApplicationRecord 
 	belongs_to :user
 	belongs_to :condominios, optional: true
-    has_many :gdrive_user_items, dependent: :destroy
+    has_many :gdrive_user_items, dependent: :delete_all
 end
