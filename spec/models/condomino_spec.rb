24370ca418c6dc7852a0aa4f55238fd75f2d6eb2
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-07-17 17:15:57 +0200

    On branch branch_testing_heroku  Changes to be committed: 	modified:   spec/models/condomino_spec.rb

--- spec/models/condomino_spec.rb
+++ spec/models/condomino_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Condomino, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:email)
+    @condominio = FactoryBot.create(:condominio)
+    @condomino = FactoryBot.create(:condomino)
+  end
+
+  describe 'creation' do
+    it 'can be created if valid' do
+      expect(@condomino).to be_valid
+    end
+    it 'will not be created if not valid user_id' do
+      @condomino.update(user_id: 243)
+      @condomino.save
+      expect(@condomino).to_not be_valid
+    end
+  end
 end
