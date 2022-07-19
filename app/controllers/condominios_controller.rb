6bebacf6223e0d8876e696ee10ba465223aa8964
Andrea Panceri <andrea.pancio00@gmail.com>
2022-07-19 10:45:07 +0200

    errori sugli eventi

--- app/controllers/condominios_controller.rb
+++ app/controllers/condominios_controller.rb
@@ -31,7 +31,7 @@ class CondominiosController < ApplicationController
     end
     @condomini = Condomino.where(condominio_id: @condominio.id)
     start_date = params.fetch(:start_date, Date.today).to_date
-    @eventi = Event.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @eventi = Event.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week,condominio_id:@condominio.id)
   end
 
   # GET /condominios/new
